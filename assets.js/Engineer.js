0660e24ffd8dbd748cfda2c1a8ea472b3cff1a0a
kendrick Brown <chefken360@gmail.com>
2022-10-23 14:04:45 -0700

    Engineer added

--- assets.js/Engineer.js
+++ assets.js/Engineer.js
@@ -2,6 +2,15 @@ const Employee = require ('./Employees');
 
 class engineer extends Employee{
     constructor({name, id, email, github}) {
-        super()
+        super(name, id, email)
+        this.github = github
+    }
+    getGithub(){
+        return this.github
+    }
+    getRole(){
+        return 'Engineer'
     }
 }
+
+module.exports = Engineer;
