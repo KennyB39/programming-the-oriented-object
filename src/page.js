480795d60784577c1ddd9e466f912351071203c3
kendrick Brown <chefken360@gmail.com>
2022-10-27 11:30:28 -0700

    debugged

--- src/page.js
+++ src/page.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
 
 function buildTeam(teamMembers){
     let page = `
